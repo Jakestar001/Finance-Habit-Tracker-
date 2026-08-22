7212360ecc454ed45de57686f8d3b48b9f1a4654
Jakestar001 <emzeeokon@gmail.com>
2026-08-22 17:54:39 +0100

    Update expense_entry.dart
    Imported http package for API integration

--- lib/expense_entry.dart
+++ lib/expense_entry.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 class ExpenseEntry extends StatefulWidget {
   @override
   _ExpenseEntryState createState() => _ExpenseEntryState();
