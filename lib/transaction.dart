bce62a3f9f4eee02e654e105ca3b3823038f02b0
Jakestar001 <emzeeokon@gmail.com>
2026-09-03 23:28:43 +0100

    Create transaction.dart

---
+++ lib/transaction.dart
@@ -0,0 +1,11 @@
+class Transaction {
+  final double amount;
+  final String type;
+  final DateTime date;
+
+  Transaction({
+    required this.amount, 
+    required this.type, 
+    required this.date,
+  });
+}
