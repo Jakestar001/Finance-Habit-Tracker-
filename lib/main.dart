f65b3fbfb0cfbb77678e6d864810673f26baf711
Jakestar001 <emzeeokon@gmail.com>
2026-09-06 13:53:44 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp();
   // Initialize Plaid and Stripe
 await PlaidManager.initialize();
 await StripeManager.initialize();
