6505528d07e57f8172e9316aca6aea647062c311
Jakestar001 <emzeeokon@gmail.com>
2026-09-06 20:18:31 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +13,7 @@ void main() async {
 await PlaidManager.initialize();
 await StripeManager.initialize();
 GetIt.I.registerSingleton<SubscriptionService>(SubscriptionService());
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(create: (_) => SubscriptionProvider(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
